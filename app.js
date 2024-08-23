bd149add0489088989b687238c3836dd1794973a
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-24 02:19:27 +0530

    Created Core React code for creating multiple elements with multiple childs

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// singleElement
 const header = React.createElement("h1", {}, "Hello World from React!!");
+// const head = ReactDOM.createRoot(document.getElementById("text"));
+// head.render(header);
+
+// MultipleElements
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1-tag"),
+    React.createElement("h2", {}, "I'm h2-tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1-tag"),
+    React.createElement("h2", {}, "I'm h2-tag"),
+  ]),
+);
+
+console.log(parent);
 const head = ReactDOM.createRoot(document.getElementById("text"));
-head.render(header);
+head.render(parent);
