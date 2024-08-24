581d3196adf77bc68e5e1aa81373c989b1252f8a
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-24 19:41:32 +0530

    Created React Element using JSX

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// singleElement
-const header = React.createElement("h1", {}, "Hello World from React!!");
-// const head = ReactDOM.createRoot(document.getElementById("text"));
-// head.render(header);
+// React ELement using JSX:
 
-// MultipleElements
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1-tag"),
-    React.createElement("h2", {}, "I'm h2-tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1-tag"),
-    React.createElement("h2", {}, "I'm h2-tag"),
-  ]),
-);
 
-console.log(parent);
-const head = ReactDOM.createRoot(document.getElementById("text"));
-head.render(parent);
+const jsxHeading = <h1 class="jsxheading"> Hello, Welcome from JSX!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("text"));
+
+root.render(jsxHeading);
