f9046674bcc6dd9f32d40dbc804026169cc4e065
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-24 22:00:54 +0530

    update

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom/client';
 // React ELement using JSX:
 
 
-const jsxHeading = <h1 class="jsxheading"> Hello, Welcome from JSX!</h1>;
+const jsxHeading = <h1 class="jsxheading" > Hello, Welcome from JSX!</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("text"));
 
 root.render(jsxHeading);
+ 
