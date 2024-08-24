5c1ea8aaeab2970fc231049f835086650c2fa483
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-25 02:04:17 +0530

    Element inside a Element

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React Element:
+// Element inside a Element!
+const welcome = <span>Welcome</span>;
+
+const element = <h2 className="blog">{welcome} to my blogs!</h2>
+
 // React Functional Component:
 const Title = () => <h1 className="title" >Title:</h1>;
 
 // React Functional Component:
-// Component Composition
+// Component Composition => Component inside a Component!
 const HeadingComponent = () => (
     <div className="container" >
         <Title />
-        <h1 className="heading">React Functional Component</h1>;   
+        <h1 className="heading">React Functional Component</h1>;
+        {element}
     </div>
 );
 
