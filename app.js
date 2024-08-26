fa523e239c23048903e5dfc712eca049991026c1
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-27 05:13:51 +0530

    integrated resCard with Swiggy API's one resData "Pizza Hut"

--- app.js
+++ app.js
@@ -23,20 +23,105 @@ const Header = () => {
 };
 
 const RestaurantCard = (props) => {
-  console.log(props);
+  const { resData } = props;
   return (
     <div className="res-card">
       <img
         className="res-logo"
-        src={props.resLogo}
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/737b12ff-5c41-46eb-af2d-a346f35cb464_10582.jpg"
       />
-      <h3 className="res-name">{props.resName}</h3>
-      <h4 className="res-cuisine"> {props.resCuisine}</h4>
-      <h4 className="res-rating"> {props.resRating}</h4>
-      <h4 className="res-eta"> {props.resEta}</h4>
-      <h4 className="res-cost"> {props.resCost}</h4>
+      <h3 className="res-name">{resData.info.name}</h3>
+      <h4 className="res-cuisine"> {resData.info.cuisines}</h4>
+      <h4 className="res-rating"> {resData.info.avgRating}</h4>
+      <h4 className="res-eta"> {resData.info.sla.deliveryTime}</h4>
+      <h4 className="res-cost"> {resData.info.costForTwo}</h4>
     </div>
   );
+};
+
+const resObj = {
+  info: {
+    id: "10582",
+    name: "Pizza Hut",
+    cloudinaryImageId:
+      "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/737b12ff-5c41-46eb-af2d-a346f35cb464_10582.jpg",
+    locality: "Maruti Nagar",
+    areaName: "Koramangala",
+    costForTwo: "₹300 for two",
+    cuisines: ["Pizzas"],
+    avgRating: 4.2,
+    parentId: "721",
+    avgRatingString: "4.2",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 32,
+      lastMileTravel: 6.8,
+      serviceability: "SERVICEABLE",
+      slaString: "30-35 mins",
+      lastMileTravelString: "6.8 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-08-27 06:00:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "ITEMS",
+      subHeader: "AT ₹189",
+    },
+    loyaltyDiscoverPresentationInfo: {
+      logoCtx: {
+        text: "BENEFITS",
+        logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+      },
+      freedelMessage: "FREE DELIVERY",
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "4.8",
+        ratingCount: "100+",
+      },
+      source: "GOOGLE",
+      sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+  },
+  analytics: {
+    context: "seo-data-d0ba5a53-6aae-4b4f-ab2d-4a3fed5853e6",
+  },
+  cta: {
+    link: "https://www.swiggy.com/restaurants/pizza-hut-maruti-nagar-koramangala-bangalore-10582",
+    text: "RESTAURANT_MENU",
+    type: "WEBLINK",
+  },
+  widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
 };
 
 const Body = () => {
@@ -44,22 +129,7 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard
-          resName="KFC"
-          resCuisine="Burgers, Rolls & Wraps"
-          resLogo="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/37dc9e37-386f-453b-a219-ffa2c154750b_347868.JPG"
-          resRating= "4.2"
-          resEta= "30-35mins"
-          resCost= "700 for two"
-        />
-        <RestaurantCard 
-        resName="Pizza Hut"
-        resCuisine="Pizzas"
-        resLogo="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/e981bc3d-6ba5-4150-8354-281f41fce767_10584.jpg"
-        resRating= "4.4"
-        resEta= "35-40mins"
-        resCost= "500 for two"
-         />
+        <RestaurantCard resData={resObj} />
       </div>
     </div>
   );
