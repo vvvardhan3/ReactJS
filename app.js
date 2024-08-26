127a914cd2d56f942727ac1ceb8af1af30156504
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-27 03:45:08 +0530

    created a restaurant card

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 const Header = () => {
   return (
     <div className="header">
       <div className="logo">
-        <img className="logo-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjXm-Lz1wqab2UY4q-vR2b1nEBH3NHEPpIsA&s"/>
+        <img
+          className="logo-img"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjXm-Lz1wqab2UY4q-vR2b1nEBH3NHEPpIsA&s"
+        />
       </div>
       <div className="nav-items">
         <ul className="list">
@@ -23,30 +25,32 @@ const Header = () => {
 const RestaurantCard = () => {
   return (
     <div className="res-card">
-      <h3>
-        //
-      </h3>
+      <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/37dc9e37-386f-453b-a219-ffa2c154750b_347868.JPG" />
+      <h3 className="res-name">KFC</h3>
+      <h4 className="res-cuisine"> American, Chicken</h4>
+      <h4 className="res-rating"> 4.4 stars</h4>
+      <h4 className="res-eta"> 30mins ETA</h4>
+      <h4 className="res-cost"> 500 for two</h4>
     </div>
-  )
+  );
 };
 
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">
-          Search
-      </div>
+      <div className="search">Search</div>
       <div className="res-container">
-        // RestaurantCards
+        <RestaurantCard />
       </div>
     </div>
-  )
-}
+  );
+};
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header></Header>
+      <Body></Body>
     </div>
   );
 };
