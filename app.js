baa61095e59aabca6fa75272fc3986c0f2b6bab8
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-28 20:07:25 +0530

    updated css and map function to render all the resCards

--- app.js
+++ app.js
@@ -951,14 +951,13 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard resData={resList[0]} />
-        <RestaurantCard resData={resList[1]} />
-        <RestaurantCard resData={resList[2]} />
-        <RestaurantCard resData={resList[3]} />
-        <RestaurantCard resData={resList[4]} />
-        <RestaurantCard resData={resList[5]} />
-        <RestaurantCard resData={resList[6]} />
-        <RestaurantCard resData={resList[7]} />
+      {
+        resList.map(
+          (restaurant) => (
+            <RestaurantCard key = {restaurant.info.id} resData = {restaurant} />
+          )
+        )
+      }
       </div>
     </div>
   );
