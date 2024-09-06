2fb8bdc000f5b572106b5dfd1c03b694887e48ae
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-09-07 00:16:37 +0530

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
+import { LOGO_URL } from "../utils/constant";
+
 const Header = () => {
     return (
       <div className="header">
         <div className="logo">
           <img
             className="logo-img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjXm-Lz1wqab2UY4q-vR2b1nEBH3NHEPpIsA&s"
+            src= {LOGO_URL}
           />
         </div>
         <div className="nav-items">
@@ -20,6 +22,5 @@ const Header = () => {
   };
 
 
-  export default {
-    Header
-  }
+  export default Header;
+  
---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjXm-Lz1wqab2UY4q-vR2b1nEBH3NHEPpIsA&s";
