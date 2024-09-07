2c018ed0d8d8fa0b9e50521bf7c28fa51fc0d947
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-09-07 10:40:34 +0530

    Introduced Shimmer UI into the Application

---
+++ src/components/ShimmerUi.js
@@ -0,0 +1,12 @@
+const ShimmerUi = () => {
+    return (
+        <div className="shimmer-container">
+            <div className="shimmer-card"> cards </div>
+            <div className="shimmer-card"> cards </div>
+            <div className="shimmer-card"> cards </div>
+            <div className="shimmer-card"> cards </div>
+        </div>
+    );
+}
+
+export default ShimmerUi;
